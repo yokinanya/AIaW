562a49aec2ea63290a84441996d5994f7c6009fe
NitroRCr <i@krytro.com>
2025-02-05 17:42:00 +0800

    fix: gemini roundtrip
    - fix: gemini roundtrip #18
    - fix: goto last dialog
    - misc: update provider sdk
    - bump: v1.3.9

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -170,6 +170,8 @@ const router = useRouter()
 async function openLastWorkspace() {
   await until(() => userDataStore.ready).toBeTruthy()
   const wsId = userDataStore.data.lastWorkspaceId
+  if (!wsId) return
+  await until(() => workspacesStore.workspaces.length).toBeTruthy()
   const dialogId = workspacesStore.workspaces.find(item => item.id === wsId)?.lastDialogId
   if (route.path === '/' && wsId) {
     router.push(dialogId ? `/workspaces/${wsId}/dialogs/${dialogId}` : `/workspaces/${wsId}`)
