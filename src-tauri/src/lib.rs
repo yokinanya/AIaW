19ffcd2e709366565a043ea563bb4b421da21ad9
NitroRCr <havechicken666@gmail.com>
2025-03-03 17:21:01 +0800

    fix: open url & build android

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,7 @@ mod stream;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![stream::stream_fetch])
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_clipboard_manager::init())
