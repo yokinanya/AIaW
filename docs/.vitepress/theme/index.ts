cf9ccbfd1011f1b5f52052ff45f482b7d54794e3
NitroRCr <i@krytro.com>
2025-05-17 20:23:40 +0800

    docs: fix redirect

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,15 +2,8 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
-export default {
-  ...DefaultTheme,
-  enhanceApp(ctx) {
-    if (DefaultTheme.enhanceApp) {
-      DefaultTheme.enhanceApp(ctx);
-    }
-
-    if (window && navigator.language.startsWith('zh') && !location.pathname.startsWith('/zh')) {
-      ctx.router.go('/zh' + location.pathname)
-    }
-  }
+if (typeof window !== 'undefined' && navigator.language.startsWith('zh') && !location.pathname.startsWith('/zh')) {
+  location.href = '/zh' + location.pathname
 }
+
+export default DefaultTheme
